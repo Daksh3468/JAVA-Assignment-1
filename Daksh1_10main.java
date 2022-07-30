c84db433507e6973387b568d0e58661dcf099929
Daksh Bangoria <96170993+Daksh3468@users.noreply.github.com>
2022-07-31 02:25:10 +0530

    Add files via upload

---
+++ Daksh1_10main.java
@@ -0,0 +1,13 @@
+// Name :- Daksh Bangoria
+// Roll-no :-21ce007
+// Aim :-Implement ceaser cipher
+// Git-hub repository:
+public class Daksh1_10main {
+    public static void main(String[] args) {
+        String text = "ATTACKATONCE";
+        int s = 4;
+        System.out.println("Text  : " + text);
+        System.out.println("Shift : " + s);
+        System.out.println("Cipher: " + Daksh1_10.encrypt(text, s));//call the encrypt function of Daksh1_10 class file.
+    }
+}
